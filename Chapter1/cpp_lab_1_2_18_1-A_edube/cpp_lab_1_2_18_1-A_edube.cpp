4901a78b2ee0062ac633e683246cfe7495b519c0
1234bpv <33402738+1234bpv@users.noreply.github.com>
2017-12-22 11:52:27 +0200

    Update cpp_lab_1_2_18_1-A_edube.cpp

--- Chapter1/cpp_lab_1_2_18_1-A_edube/cpp_lab_1_2_18_1-A_edube.cpp
+++ Chapter1/cpp_lab_1_2_18_1-A_edube/cpp_lab_1_2_18_1-A_edube.cpp
@@ -1,75 +1,27 @@
 #include <iostream>
-#include <string>
-#include <deque>
+#include <list>
 
 using namespace std;
 
-enum CommandType
-{
-	MoveLeft,
-	MoveRight,
-	MoveForward,
-	MoveBackward
-};
-
-string CommandTypeToString(CommandType cmdType)
-{
-	string str = "";
-	switch(cmdType)
-	{
-		case MoveLeft: str = "Moving left"; break;
-		case MoveRight: str = "Moving right"; break;
-		case MoveForward: str = "Moving forward"; break;
-		case MoveBackward: str = "Moving backward"; break;
-	}
-	return str;
-}
-
-class RoboCommander
-{
-private:
-	deque<CommandType> d1;
-public:
-	void AddCommand(CommandType cmdType)
-	{
-		d1.push_back(cmdType);
-	}
-	void UndoCommand()
-	{
-		if(d1.size() != 0)
-			d1.pop_back();
-	}
-	void Execute()
-	{
-		for (CommandType d: d1)
-		{
-			cout << CommandTypeToString(d) << endl;
-		}
-		cout << "Ready" << endl;
-	}
-};
-
 int main()
 {
-	RoboCommander commander;
-	commander.AddCommand(MoveLeft);
-	commander.AddCommand(MoveRight);
-	commander.UndoCommand();
-	commander.UndoCommand();
-	commander.UndoCommand();
-	commander.AddCommand(MoveLeft);
-	commander.AddCommand(MoveForward);
-	commander.AddCommand(MoveLeft);
-	commander.AddCommand(MoveForward);
-	commander.AddCommand(MoveRight);
-	commander.AddCommand(MoveBackward);
-	commander.Execute();
-	commander.UndoCommand();
-	commander.UndoCommand();
-	commander.UndoCommand();
-	commander.UndoCommand();
-	commander.AddCommand(MoveForward);
-	commander.Execute();
-	system("pause");
+	list<int> elements1;
+	elements1.push_back(1);
+	elements1.push_back(2);
+	elements1.push_back(3);
+	list<int> elements2;
+	elements2.push_back(5);
+	elements2.push_back(8);
+	elements2.push_back(6);
+	cout << elements1.front() + elements2.front() << " ";
+	elements1.remove(2);
+	elements2.remove(8);
+	cout << elements1.back() + elements2.back() << " ";
+	elements1.pop_back();
+	elements2.pop_back();
+	elements2.insert(elements2.begin(), 7);
+	cout << elements1.back() + elements2.back() << " ";
+	elements2.pop_back();
+	cout << elements1.back() + elements2.back() << " ";
 	return 0;
 }
